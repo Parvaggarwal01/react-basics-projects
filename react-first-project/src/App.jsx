eac86666df24fb056affa65883f76bd1d55281c0
Parvaggarawal01 <parvaggarwal130@gmail.com>
2024-05-30 17:32:17 +0530

    Complete User Card Project

--- react-first-project/src/App.jsx
+++ react-first-project/src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react"
+import React from "react";
+import "./user.css";
+import user from "./user.json";
+import { UserCard } from "./UserCard";
 
 function App() {
-  return 
+  return (
+    <div>
+      <UserCard
+        name={user.name}
+        phoneNumber={user.phoneNumber}
+        age={user.age}
+        address={user.address}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
